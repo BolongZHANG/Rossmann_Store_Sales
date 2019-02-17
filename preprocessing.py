265193c2b15dd4f3208802a7df6d1c0fad07a2d4
SWKG21 <kevin.wang.sun@gmail.com>
2019-02-17 19:04:51 +0100

    modif all

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from datetime import datetime
 
 
 def preprocess(df, store):
     df = pd.merge(df, store, on='Store', how='left')
-
-    for i in range(1, 8):
-        df['DayOfWeek_'+str(i)] = (df['DayOfWeek'] == i).astype(float)
         
-    df['DaysSince20150731'] = df['Date'].map(lambda x: (datetime.strptime(x, "%Y-%m-%d") - datetime.strptime("2015-07-31", "%Y-%m-%d")).days)
+    df['Date'] = df['Date'].map(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    # df['DaysSince20150731'] = df['Date'].map(lambda x: (datetime.strptime(x, "%Y-%m-%d") - datetime.strptime("2015-07-31", "%Y-%m-%d")).days)
+        
+    day2str = {1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat', 7:'SUn'}
+    for i in range(1, 8):
+        df[day2str[i]] = (df['DayOfWeek'] == i).astype(float)
         
     df['StateHoliday'] = df['StateHoliday'].astype(str)
     for i in ['0', 'a', 'b', 'c']:
@@ -24,10 +27,49 @@ def preprocess(df, store):
     df['PromoInterval'] = df['PromoInterval'].astype(str)
     for mon in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']:
         df['PromoInterval_'+mon] = df['PromoInterval'].map(lambda x: float(mon in x))
-        
-    df.drop(columns=['DayOfWeek', 'Date', 'StateHoliday', 'StoreType', 'Assortment', 'PromoInterval'], inplace=True)
-    df.fillna(-1, inplace=True)
+
+    return df
+
+
+def add_feature(df):
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day  # day in month
+    df['WeekOfYear'] = df['Date'].dt.weekofyear
     
+    # whether competition is currently open and it started how many months ago
+    df['CompetitionOpenMonthsAgo'] = 12 * (df['Year'] - df['CompetitionOpenSinceYear']) + (df['Month'] - df['CompetitionOpenSinceMonth'])
+    df['CompetitionOpen'] = df['CompetitionOpenMonthsAgo'].apply(lambda x: x if x > 0 else 0)
+    
+    # whether Promo2 is currently open and it started how many months ago
+    df['Promo2OpenMonthsAgo'] = 12 * (df['Year'] - df['Promo2SinceYear']) + (df['WeekOfYear'] - df['Promo2SinceWeek']) / 4.0
+    df['Promo2Open'] = df['Promo2OpenMonthsAgo'].apply(lambda x: x if x > 0 else 0)
+    df.loc[df['Promo2SinceYear']==0, 'Promo2Open'] = 0
+
+    # whether PromoInterval is currently open
+    month2str = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sept', 10:'Oct', 11:'Nov', 12:'Dec'}
+    def checkPromoInterval(x):
+        mon = month2str[x['Month']]
+        return float(x['PromoInterval_'+mon]==1)
+    df['IsPromoIntervalMonth'] = df.apply(checkPromoInterval, axis=1)
+
+    df['TueToFri'] = ((df['DayOfWeek'] > 1) & (df['DayOfWeek'] < 6)).astype(float)
+
+    # whether it is currently open and it is currently holiday
+    df['OpenHoliday'] = np.where((df['Open'] == 1) & ((df['SchoolHoliday'] == 1)|(df['StateHoliday_0'] != 1)), 2, 1)
+    df.loc[df['Open']==0,'OpenHoliday'] = 0
+    
+    return df
+
+
+def fill_and_drop(df):
+    df['CompetitionDistance'].fillna(100000, inplace=True)
+    df['CompetitionOpenMonthsAgo'].fillna(0, inplace=True)
+    df['Promo2OpenMonthsAgo'].fillna(0, inplace=True)
+    
+    df.drop(columns=['Store', 'DayOfWeek', 'StateHoliday', 'StoreType', 'Assortment', 'PromoInterval', 'Date'], inplace=True)
+    df.drop(columns=['CompetitionOpenSinceYear', 'CompetitionOpenSinceMonth', 'Promo2SinceYear', 'Promo2SinceWeek'], inplace=True)
+
     return df
 
 
@@ -36,5 +78,11 @@ if __name__ == "__main__":
     train = pd.read_csv("data/train.csv")
     test = pd.read_csv("data/test.csv")
     store = pd.read_csv("data/store.csv")
+    
     df_train = preprocess(train, store)
+    df_train = add_feature(df_train)
+    df_train = fill_and_drop(df_train)
+    
     df_test = preprocess(test, store)
+    df_test = add_feature(df_test)
+    df_test = fill_and_drop(df_test)
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from preprocessing import preprocess
 import pandas as pd
 import numpy as np
 from joblib import dump, load
-import xgboost as xgb
+from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import make_scorer
@@ -19,9 +19,9 @@ def rmspe(y_true, y_pred):
 
 
 def cross_validation(clf, X, Y, n):
-	scorer = make_scorer(rmspe)
-	scores = cross_val_score(clf, X, Y, cv=n, scoring=scorer)
-	return scores
+    scorer = make_scorer(rmspe)
+    scores = cross_val_score(clf, X, Y, cv=n, scoring=scorer)
+    return scores
 
 
 if __name__ == "__main__":
@@ -37,10 +37,13 @@ if __name__ == "__main__":
     y_train = df_train['Sales']
     X_test = df_test.drop(['Id', 'Store'], axis=1)
 
-    clf_1 = RandomForestRegressor(max_depth=8, random_state=0, n_estimators=50, oob_score=True)
-    clf_1.fit(X_train, y_train)
-    dump(clf_1, 'model/randomForest.joblib')
-
-    clf_2 = xgb.XGBRegressor(max_depth=8, n_estimators=100, early_stopping_rounds=50)
-    clf_2.fit(X_train, y_train)
-    dump(clf_2, 'model/XGboost.joblib')
+    # rf = RandomForestRegressor(max_depth=8, random_state=0, n_estimators=50, oob_score=True)
+    # rf.fit(X_train, y_train)
+    # print (sorted(list(zip(X_train.columns, rf.feature_importances_)), key=lambda x: x[1]))    
+    
+    xgb = XGBRegressor(max_depth=10, n_estimators=500, subsample=0.9, colsample_bytree=0.7, early_stopping_rounds=100)
+    xgb.fit(X_train, y_train)
+    scores = cross_validation(xgb, X_train, y_train, 5)
+    print (scores)
+    print (np.mean(scores))
+    # dump(clf_2, 'model/XGboost.joblib')
